8633a7aad727078b441e056ac4f2c715822bb440
zicotann <zicotann@hotmail.com>
2024-12-09 01:37:20 +0800

    chore: clean up app vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+  <!-- <header>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <h1 class="text-5xl font-bold">Title</h1>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
 
   <RouterView />
 </template>
