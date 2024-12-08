e0ece2e2481d33d08d7e5544a19776d5ed9c1a3a
zicotann <zicotann@hotmail.com>
2024-12-09 01:38:00 +0800

    add: location card to weather list

--- src/views/WeatherListView.vue
+++ src/views/WeatherListView.vue
@@ -1,10 +1,21 @@
 <template>
-    <div>
-        <p>List of weather locations</p>
+    <div class="container mx-auto">
+        <div class="flex flex-col space-y-4">
+            <div class="flex justify-between items-center">
+                <h1 class="text-3xl font-bold">Weather</h1>
+                <p>Profile icon</p>
+            </div>
+
+            <div class="card">
+                <LocationCard />
+            </div>
+
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { LocationCard } from "@/components";
 
 </script>
 
