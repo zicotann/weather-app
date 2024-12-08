01239a0035e010d59c6467d4087ce6e9b098aa8e
zicotann <zicotann@hotmail.com>
2024-12-08 17:14:18 +0800

    chore: add tailwind

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
-      <h1>Title</h1>
+      <h1 class="text-5xl font-bold">Title</h1>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
