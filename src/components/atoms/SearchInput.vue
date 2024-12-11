0676af0d4d2575dd6aadf7ddf95b3abfaf20e8a4
zicotann <zicotann@hotmail.com>
2024-12-11 21:58:20 +0800

    add: search input component

---
+++ src/components/atoms/SearchInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <input type="text" placeholder="Search" class="bg-gray-100 rounded-md px-4 py-2 focus:outline-none">
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
