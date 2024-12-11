6418720fce7c16e55a71cf8a2945b6efb6794304
zicotann <zicotann@hotmail.com>
2024-12-11 21:59:17 +0800

    add: weekly forcast component

---
+++ src/components/WeeklyForecastCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex justify-between items-center rounded-xl p-4 bg-blue-200">
+        <div class="flex gap-4">
+            <span>Icon</span>
+            <div>
+                <p>Monday</p>
+                <p>Sunny</p>
+            </div>
+
+        </div>
+        <div>
+            <span>30ºC</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
