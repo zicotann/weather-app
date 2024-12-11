947fcd9cf718a739da134796100b1b3eafa11245
zicotann <zicotann@hotmail.com>
2024-12-11 21:59:03 +0800

    add: hourly forecast component

---
+++ src/components/HourlyForecastCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex flex-col rounded-sm p-4 bg-slate-100">
+        <p class="font-bold">18º</p>
+        <span>12:00 PM</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
