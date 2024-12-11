bdc002aa1a670b695a86ba6986739e90fda15677
zicotann <zicotann@hotmail.com>
2024-12-11 22:58:03 +0800

    add: weather listing scaffolding

--- src/views/WeatherListView.vue
+++ src/views/WeatherListView.vue
@@ -3,8 +3,12 @@
         <div class="flex flex-col space-y-4">
             <div class="flex justify-between items-center">
                 <h1 class="text-3xl font-bold">Weather</h1>
-                <p>Profile icon</p>
+                <img src="@/assets/icons/profile.svg" alt="">
             </div>
+
+            <RouterLink to="search">
+                <button>Search</button>
+            </RouterLink>
 
             <div class="card">
                 <LocationCard />
@@ -16,6 +20,7 @@
 
 <script setup lang="ts">
 import { LocationCard } from "@/components";
+import { RouterLink } from 'vue-router';
 
 </script>
 
