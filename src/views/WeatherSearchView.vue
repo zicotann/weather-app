103864c2a0999fe77da75d42560d1629cc585e47
zicotann <zicotann@hotmail.com>
2024-12-11 22:59:02 +0800

    add: create weather search page

--- src/views/WeatherSearchView.vue
+++ src/views/WeatherSearchView.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-
+    <div class="flex flex-wrap items-center gap-4">
+        <input type="text" placeholder="Search" class="bg-gray-100 flex-grow">
+        <RouterLink :to="{name: 'weather'}" >Cancel</RouterLink>
     </div>
 </template>
 
