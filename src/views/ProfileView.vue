89d3b06aeb41cde0c8b593ea41781a6e96788614
zicotann <zicotann@hotmail.com>
2024-12-11 23:08:34 +0800

    add: create profile page

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,10 +1,15 @@
 <template>
-    <div>
-
+    <div class="flex flex-wrap justify-between items-center">
+        <RouterLink :to="{ name: 'weather'}">
+            <img src="@/assets/icons/back-black.svg" alt="" class="h-auto w-3">
+        </RouterLink>
+        <p class="font-bold text-xl">Edit profile</p>
+        <div></div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { RouterLink } from 'vue-router';
 
 </script>
 
