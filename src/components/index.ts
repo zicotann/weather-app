fda051be34d87c77ad2712f5f5610b8fe1620d31
zicotann <zicotann@hotmail.com>
2024-12-11 21:59:33 +0800

    add: export for for cards

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as LocationCard } from './LocationCard.vue'
+export { default as HourlyForecastCard } from './HourlyForecastCard.vue'
+export { default as WeeklyForecastCard } from './WeeklyForecastCard.vue'
